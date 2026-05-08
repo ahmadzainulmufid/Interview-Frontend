781131ced24994e6c710fb3623a5640b35b92db2
Ahmad Zainul Mufid <ahmadzainulmufid12@gmail.com>
2026-05-08 15:04:05 +0700

    fix: deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React, { StrictMode } from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
